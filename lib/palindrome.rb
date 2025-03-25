4d2eee88af4b048e4ff07388b8890128eacb3041
yungProg <122945393+yungProg@users.noreply.github.com>
2025-03-25 12:16:19 +0000

    Remove comment

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,8 +1,4 @@
 def palindrome(word)
-  #first letter == last
-  #if so, remove both
-  #return false if there is a mismatch
-  #else return true
   return true if word.length <= 1
   if word[0] == word[-1]
     palindrome(word[1..-2])
